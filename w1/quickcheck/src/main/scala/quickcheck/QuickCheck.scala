2ab8fd1b1299e084338d9e92644633c688a31c00
Marcel Arrufat <marrufat@zinio.com>
2016-12-23 15:23:40 +0100

    temp commit for quickcheck

--- w1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ w1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,11 +1,8 @@
 package quickcheck
 
-import common._
-
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
 import org.scalacheck._
-import Arbitrary._
-import Gen._
-import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -14,7 +11,28 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("gen1") = forAll { (h: H) =>
+
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  property("min2") = forAll { (a: Int, b: Int) =>
+
+    if (a < b) {
+      val h = insert(a, empty)
+      findMin(h) == a
+    } else if (b < a) {
+      val h = insert(b, empty)
+      findMin(h) == b
+    } else true
+  }
+
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[Int]
+    m <- Gen.oneOf[H](empty, genHeap)
+  } yield insert(k, m)
+
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
