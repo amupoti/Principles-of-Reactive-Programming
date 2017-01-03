bd7b0d3a010edd90982c7099ab65d032b4fd0105
Marcel Arrufat <marrufat@zinio.com>
2017-01-03 12:02:33 +0100

    Added final solution

--- w1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ w1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -11,29 +11,70 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  property("gen1") = forAll { (h: H) =>
+  property("gen1 (Bogus 1)") = forAll { (h: H) =>
 
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
 
-  property("min2") = forAll { (a: Int, b: Int) =>
 
-    if (a < b) {
-      val h = insert(a, empty)
-      findMin(h) == a
-    } else if (b < a) {
-      val h = insert(b, empty)
-      findMin(h) == b
-    } else true
+
+  property("meld and find min (Bogus 5)") = {
+    val a = 1
+    val h = insert(a, empty)
+    val b = 0
+    val h2 = insert(b, empty)
+
+    findMin(meld(h, h2)) == 0
   }
+
+  property("insert and remove  must be empty") = forAll { (a: Int) =>
+
+    val h = insert(a, empty)
+
+    deleteMin(h) == empty
+
+  }
+
+
+  property("insert 3, delete and find min (Bogus 4)") = {
+
+    val h = insert(66,insert(44,insert(55, empty)))
+    findMin(deleteMin(h)) == 55
+  }
+
+  property("Sorted mins") = forAll { (list: List[Int]) =>
+
+
+    var h = empty
+    list.foreach(i => {
+      h = insert(i, h)
+    })
+
+    val e = isEmpty(h)
+
+    if (e) true
+    else {
+      val min = findMin(h)
+      e || checkOrder(deleteMin(h), min, true)
+    }
+
+  }
+
+  def checkOrder(h: H, last: Int, b: Boolean): Boolean = {
+
+    if (isEmpty(h)) b
+    else {
+      val currentMin = findMin(h)
+      checkOrder(deleteMin(h), currentMin, currentMin >= last & b)
+    }
+
+  }
+
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   lazy val genHeap: Gen[H] = for {
     k <- arbitrary[Int]
     m <- Gen.oneOf[H](empty, genHeap)
   } yield insert(k, m)
-
-
-  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
-
 }
